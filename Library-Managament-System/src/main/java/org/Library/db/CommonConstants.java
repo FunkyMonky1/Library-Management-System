caa74a60c80d7375efdf951ec96d1c1e812b4d25
FunkyMonky1 <159067017+FunkyMonky1@users.noreply.github.com>
2025-11-22 16:06:27 +0100

    Delete Library-Managament-System directory

--- Library-Managament-System/src/main/java/org/Library/db/CommonConstants.java
+++
@@ -1,9 +0,0 @@
-package org.Library.db;
-
-public class CommonConstants {
-    public static final String URL = "jdbc:mysql://127.0.0.1:3306/books_schema";
-    public static final String USER = "root";
-    public static final String PASSWORD = "Rapunzel1108+";
-    public static final String DB_KUNDE_TABLE_NAME = "Kunde";
-    public static final String DB_BOOKS_TABLE_NAME = "Books";
-}
